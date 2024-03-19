01218b9f2ecd5c8ffc5703ceba5393b2d34b7f01
why39charactersonlybruh <rick.gregoire9@gmail.com>
2024-03-19 16:07:27 -0400

    a whole lot of stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,20 @@
 	let todoList = [];
 
 	function addToArray() {
-		todoList.push(todoItem);
+		if (todoItem == '') {
+			return;
+		}
+		todoList = [...todoList, {
+			text: todoItem,
+			done: false
+		}];
 		todoList = todoList;
 		console.log(todoList);
 		todoItem = '';
+	}
+	function removeThis(index) {
+		todoList.splice(index, 1);
+		todoList = todoList;
 	}
 
 </script>
@@ -20,7 +30,31 @@
 </form>
 
 <ul>
-
+	{#each todoList as item, index}
+		<li>
+			<input type="checkbox" bind:checked={item.done}>
+			<span class:done={item.done} >{item.text}</span>
+			<span on:click={() => removeThis (index)} 
+			class="remove" role="button" tabindex="0">&times;</span>
+		</li>
+	{/each}
 </ul>
 
+<style>
+	ul {
+		list-style: none;
+	}
+	li {
+		font-size:  1.3rem;
+	}
+	.done {
+		color: red;
+		text-decoration: line-through;
+	}
+	.remove {
+		color: red;
+		cursor: pointer;
+
+	}
+</style>
 
