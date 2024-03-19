3cfc9747ee98135a2949991d4574a7e83523b915
why39charactersonlybruh <rick.gregoire9@gmail.com>
2024-03-19 14:55:04 -0400

    changed page svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,26 @@
 <script>
-	import '../style.css'
+	import '../style.css';
+	let todoItem = '';
+	let todoList = [];
+
+	function addToArray() {
+		todoList.push(todoItem);
+		todoList = todoList;
+		console.log(todoList);
+		todoItem = '';
+	}
+
 </script>
 
 <h1>My Todo App!</h1>
 
-<form>
-	<input type="text">
+<form on:submit|preventDefault={addToArray}>
+	<input type="text" bind:value={todoItem}>
 	<button type="submit">Add</button>
 </form>
 
 <ul>
-	
+
 </ul>
 
 
